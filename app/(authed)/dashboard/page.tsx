313b3c1930f37e3a2d1e90945c85b893031d9777
tperodi <perodithomas88@gmail.com>
2025-07-28 19:55:03 +0200

    dashboard divise per ruolo

--- app/(authed)/dashboard/page.tsx
+++ app/(authed)/dashboard/page.tsx
@@ -1,13 +1,64 @@
 // app/dashboard/page.tsx (Server Component)
+"use client"
 import MerchAdminDashboard from '@/components/dashboard/Merch/Dashboard';
+import { getUtenteCompleto } from '@/lib/api';
+import { useEffect, useState } from "react";
+import toast from 'react-hot-toast';
 
-export default async function DashboardPage() {
+type Utente = {
+  id: string;
+  nome: string;
+  email: string;
+  classe: string;
+  ruolo: string;
+  notifiche: boolean;
+  tema: string;
+  scuola: { id: string; nome: string } | null;
+  scuola_nome: string | null;
+};
 
-  
+export default function DashboardPage() {
+    
 
+    // Verifica il Ruolo dell'Utente e mostra il dashboard in base al ruolo
+    const [utente, setUtente] = useState<Utente | null>(null);
+    useEffect(() => {
+        const fetchUtente = async () => {
+            try {
+                const utente = await getUtenteCompleto();
+                setUtente(utente);
+            } catch  {
+                toast.error("Errore nel recupero dei dati utente");
+            }
+        };
+        fetchUtente();
+    }, []);
+    if (!utente) {
+        return <div>Caricamento...</div>;
+    }
+    else if (utente.ruolo == "merch") {
+        
+        return <MerchAdminDashboard />;
+    }
+    else if (utente.ruolo === "studente" ) {
+
+        return <div>Dashboard Studente in costruzione</div>
+    }
+    else if (utente.ruolo === "admin") {
+        return <div>Dashboard Admin in costruzione</div>
+    }
+    else if (utente.ruolo === "docente") {
+        return <div>Dashboard Docente in costruzione</div>
+    }
+    // Renderizza il dashboard per admin e merch
+
+
+
+
+    
   
     
-      return <MerchAdminDashboard />;
+      
    
    
   }
