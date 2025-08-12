0992c44b91f04c8f1cb9afa1db9f83c3ae5bcee4
tperodi <perodithomas88@gmail.com>
2025-08-12 19:33:45 +0200

    fix bug

--- app/(authed)/merchandising/[productId]/page.tsx
+++ app/(authed)/merchandising/[productId]/page.tsx
@@ -1,26 +1,11 @@
-// app/(authed)/merchandising/[productId]/page.tsx
-
-import ProductDetailMerch from "./productClientMerch"
+import ProductDetailMerch from "./productClientMerch";
 
 export const metadata = {
   title: "Skoolly - Merchandising - Dettaglio Prodotto",
   description: "Scopri il merchandising della tua scuola - Dettaglio Prodotto",
   keywords: "merchandising, scuola, prodotti ufficiali, abbigliamento scolastico, accessori",
-    
-    
 };
 
-interface PageProps {
-  params: {
-    productId: string;
-  };
-}
-
-export default function Page({ params }: PageProps) {
-  // params.productId è la stringa dall'url
-
-  // Qui puoi fare fetch dati server-side se vuoi
-  // oppure semplicemente passare il productId al componente client
-
+export default function ProductPageMerch({ params }: { params: { productId: string } }) {
   return <ProductDetailMerch productId={params.productId} />;
 }
